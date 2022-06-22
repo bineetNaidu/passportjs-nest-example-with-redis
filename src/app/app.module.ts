747381ce97234fbc63652e87e3ab6d2e02c2d026
bineetNaidu <bineet.s.naidu@gmail.com>
2022-06-22 15:23:03 +0530

    🎱 add express-session

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import * as session from 'express-session';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AuthModule } from '../auth/auth.module';
@@ -8,8 +9,28 @@ import { AuthModule } from '../auth/auth.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+      context: ({ req, res }) => ({ res, req }),
     }),
     AuthModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        session({
+          secret: process.env.SESSION_SECRET,
+          resave: false,
+          saveUninitialized: false,
+          name: 'passportjs_nest_example_with_redis_cookie',
+          cookie: {
+            maxAge: 1000 * 60 * 60 * 24,
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: false,
+          },
+        }),
+      )
+      .forRoutes('/graphql');
+  }
+}
