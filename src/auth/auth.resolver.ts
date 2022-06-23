92ece3e31763fa4a20b8a8b7e8a43c4f6808ea42
bineetNaidu <bineet.s.naidu@gmail.com>
2022-06-23 08:40:55 +0530

    ✨ add me and logout resolvers with session

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,24 +1,36 @@
-import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
+import { Resolver, Mutation, Args, Query, Context } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { Auth } from './entities/auth.entity';
 import { RegisterAndLoginInput } from './dto/register.input';
+import { MyContext } from 'src/utils/types';
 
 @Resolver(() => Auth)
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
   @Mutation(() => Auth)
-  login(@Args('data') data: RegisterAndLoginInput): Promise<Auth> {
-    return this.authService.login(data);
+  login(
+    @Args('data') data: RegisterAndLoginInput,
+    @Context() ctx: MyContext,
+  ): Promise<Auth> {
+    return this.authService.login(data, ctx);
   }
 
   @Mutation(() => Auth)
-  register(@Args('data') data: RegisterAndLoginInput): Promise<Auth> {
-    return this.authService.register(data);
+  register(
+    @Context() ctx: MyContext,
+    @Args('data') data: RegisterAndLoginInput,
+  ): Promise<Auth> {
+    return this.authService.register(data, ctx);
   }
 
-  @Query(() => String)
-  hello() {
-    return 'Hello World!';
+  @Query(() => Auth, { nullable: true })
+  me(@Context() ctx: MyContext): Promise<Auth | null> {
+    return this.authService.me(ctx);
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Context() ctx: MyContext): Promise<boolean> {
+    return this.authService.logout(ctx);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,12 +8,13 @@ import { RegisterAndLoginInput } from './dto/register.input';
 import { hash, verify } from 'argon2';
 import { Auth } from './entities/auth.entity';
 import { PrismaService } from 'src/services/prisma/prisma.service';
+import { MyContext } from 'src/utils/types';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async login(data: RegisterAndLoginInput): Promise<Auth> {
+  async login(data: RegisterAndLoginInput, ctx: MyContext): Promise<Auth> {
     const existingUser = await this.prisma.user.findUnique({
       where: {
         email: data.email,
@@ -26,11 +27,11 @@ export class AuthService {
     if (!isValid) {
       throw new UnauthorizedException();
     }
-
+    ctx.req.session.userId = existingUser.id;
     return existingUser;
   }
 
-  async register(data: RegisterAndLoginInput): Promise<Auth> {
+  async register(data: RegisterAndLoginInput, ctx: MyContext): Promise<Auth> {
     const existingUser = await this.prisma.user.findUnique({
       where: {
         email: data.email,
@@ -47,6 +48,22 @@ export class AuthService {
         password: hashedPassword,
       },
     });
+    ctx.req.session.userId = user.id;
     return user;
+  }
+
+  async me(ctx: MyContext): Promise<Auth | null> {
+    if (!ctx.req.session.userId) return null;
+    return this.prisma.user.findUnique({
+      where: {
+        id: ctx.req.session.userId,
+      },
+    });
+  }
+
+  async logout(ctx: MyContext): Promise<boolean> {
+    if (!ctx.req.session.userId) return false;
+    ctx.req.session.userId = null;
+    return true;
   }
 }
